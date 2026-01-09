d1417de11c413566f74a24bf32d955aeef622283
ShayThorn97 <shaythorn@hotmail.co.uk>
2026-01-09 00:03:19 +0000

    gonna make it like stb libs where they are single file

--- bnkstring.h
+++ bnkstring.h
@@ -2,7 +2,9 @@
 
 #include <stdbool.h>
 
-#include "bnkalloc.h"
+#define BNKSTRINGDEF static inline
+
+typedef struct Arena Arena;
 
 typedef struct {
     const char* data;
@@ -22,3 +24,11 @@ String StringAppend(Arena* buf, String s1, String s2);
 
 char* StringToCString(Arena* arena, String s);
 size_t StringCStrSize(String s);
+
+#ifdef BNK_STRING_IMPLEMENTATION
+
+BNKSTRINGDEF String StringFrom(String s, int from) { return (String){0}; }
+BNKSTRINGDEF String StringTo(String s, int to) { return (String){0}; }
+BNKSTRINGDEF String StringFromTo(String s, int to) {return (String){0}; }
+
+#endif
