9f6df3ebbde75a2b4cbc10e797655698925c2f86
ShayThorn97 <shaythorn@hotmail.co.uk>
2026-01-12 21:50:29 +0000

    changed how the arena allocator worked

--- bnkalloc.h
+++ bnkalloc.h
@@ -1,29 +1,68 @@
 #pragma once
 
 #include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
 
-#include "bnktypes.h"
+#define bnk_KB(x) ((x) * 1024)
+#define bnk_MB(x) ((x) * (1024 * 1024))
+#define bnk_GB(x) ((uint64_t)(x) * (1024 * 1024 * 1024))
 
-#define KB(x) ((x) * 1024)
-#define MB(x) ((x) * (1024 * 1024))
-#define GB(x) ((u64)(x) * (1024 * 1024 * 1024))
+typedef struct {
+    uint8_t* data;
+    uint64_t offset;
+    uint64_t cap;
+} bnk_Arena;
 
-typedef struct Arena Arena;
-struct Arena {
-    char* data;
-    u64 cap;
-    u64 pos;
-};
+bnk_Arena bnk_new_arena(uint64_t size);
+void bnk_arena_delete(bnk_Arena* a);
+void bnk_arena_reset(bnk_Arena* a);
 
-Arena ArenaCreate(u64 size);
-void ArenaDelete(Arena* a);
+void* bnk_arena_push(bnk_Arena* a, uint64_t size, uint64_t align);
+void* bnk_arena_push_zero(bnk_Arena* a, uint64_t size, uint64_t align);
 
-void* ArenaPush(Arena* a, u64 size, u64 alignment);
-void* ArenaPushZero(Arena* arena, u64 size, u64 alignment);
+#define bnk_arena_push_array(a, t, n) bnk_arena_push((a), sizeof(t) * (n), _Alignof(t))
+#define bnk_arena_push_struct(a, t) bnk_arena_push_array((a), t, 1)
+#define bnk_arena_push_array_zero(a, t, n) bnk_arena_push_zero((a), sizeof(t) * (n), _Alignof(t))
+#define bnk_arena_push_struct_zero(a, t) bnk_arena_push_array_zero((a), t, 1)
 
-#define ArenaPushArray(a, t, n) ArenaPush((a), sizeof(t) * (n), _Alignof(t))
-#define ArenaPushArrayZero(a, t, n) ArenaPushZero((a), sizeof(t) * (n), _Alignof(t))
-#define ArenaPushStruct(a, t) ArenaPushArray((a), t, 1)
-#define ArenaPushStructZero(a, t) ArenaPushArrayZero((a), t, 1)
+#ifdef BNK_ALLOC_IMPLEMENTATION
 
-void ArenaReset(Arena* arena);
+// need to implement VirtualAlloc and mmap instead of pants malloc
+bnk_Arena bnk_new_arena(uint64_t size) {
+    return (bnk_Arena){
+        .data = malloc(size),
+        .cap = size
+    };
+}
+
+void bnk_arena_delete(bnk_Arena* a) {
+    if (a->data) free(a->data);
+}
+
+void bnk_arena_reset(bnk_Arena* a) {
+    a->offset = 0; 
+}
+
+void* bnk_arena_push(bnk_Arena* a, uint64_t size, uint64_t align) {
+    uint64_t padding = -a->offset & (align - 1); 
+    if (a->offset + size + padding > a->cap) return NULL;
+
+    void* ptr = a->data + a->offset + padding;
+    a->offset += size + padding;
+
+    return ptr;
+}
+
+void* bnk_arena_push_zero(bnk_Arena* a, uint64_t size, uint64_t align) {
+    uint64_t padding = -a->offset & (align - 1); 
+    if (a->offset + size + padding > a->cap) return NULL;
+
+    void* ptr = a->data + a->offset + padding;
+    a->offset += size + padding;
+
+    memset(ptr, 0, size);
+    return ptr;
+}
+
+#endif
