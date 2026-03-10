74515174e5343cf5644e93327e5b7356245f1e20
ShayThorn97 <shaythorn@hotmail.co.uk>
2026-03-10 00:52:04 +0000

    got rid of bnkarray.h

--- bnkarray.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stdlib.h>
-
-#define bnk_Array(T) struct { T* data; int len; int cap; }
-#define bnk_append(arr, el) arr.data[arr.len++] = el
-
-#define bnk_new_array(arr, N) (__typeof__(arr)){ .data = malloc(N * sizeof(*arr.data)), .cap = N }
